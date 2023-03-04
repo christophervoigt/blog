297a50f601a1202ef9f42e6cdc6994426b6a6356
Christopher <chlorophyllkid@gmail.com>
2023-03-04 07:58:33 +0100

    remove typescript, add prettier and format all the things

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,4 @@
+// This holds definition for astro:content, which is a module created by Astro during build time.
+// It is used to import `getCollection` for rss and blog posts. And `defineCollection` and `z` (?) for content schemas.
+
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
