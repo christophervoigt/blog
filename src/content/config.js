69fd603b2971472df3b50e96a17b6f75c4b3f9aa
Christopher <chlorophyllkid@gmail.com>
2024-05-12 07:17:44 +0200

    fix blog collection schema by adding hasCode as boolean

--- src/content/config.js
+++ src/content/config.js
@@ -14,6 +14,7 @@ const blog = defineCollection({
       .string()
       .optional()
       .transform((str) => (str ? new Date(str) : undefined)),
+    hasCode: z.boolean().optional(),
   }),
 })
 
